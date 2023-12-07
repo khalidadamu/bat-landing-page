9d5dd71b1d5eb0017fa7450e8065330534fbbb74
Khalid <khalidbox720@icloud.com>
2023-12-07 11:05:35 +0100

    hjhj

---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+document.getElementById("our_services").scrollIntoView({ behavior: "smooth" });
+document.getElementById("service_map").scrollIntoView({ behavior: "smooth" });
